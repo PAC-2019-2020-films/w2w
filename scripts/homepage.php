b5cf21fac6c8271a1d84d26d8d5520bb370dae6a
Naelle <roost.laura@gmail.com>
2020-01-22 23:23:03 +0100

    Update Layout

--- scripts/homepage.php
+++ scripts/homepage.php
@@ -7,19 +7,32 @@
 use \w2w\DAO\DAOFactory;
 
 
-$lastMoviesNumber = 3;
+$lastMoviesSliderNumber = 3;
+$lastMoviesNumber = 6;
+
 $popularMoviesNumber = 10;
+
+$w2wPopularMoviesNumber = 5;
+
 
 $daoFactory = DAOFactory::getDAOFactory();
 
 $movieDAO = $daoFactory->getMovieDAO();
+
+// Last Movies
+
+$lastMoviesSlider = $movieDAO->findLast($lastMoviesSliderNumber);
 $lastMovies = $movieDAO->findLast($lastMoviesNumber);
+
+// Popular
 $popularMovies = $movieDAO->findBest($popularMoviesNumber);
+$firstPopularMovie = $movieDAO->findBest(1);
+$w2wPopularMovie = $movieDAO->findBest($w2wPopularMoviesNumber);
 
+$tagDAO = $daoFactory->getTagDAO();
+$tags = $tagDAO->findAll();
 
-//$tagDAO = $daoFactory->getTagDAO();
-//$tags = $tagDAO->findAll();
+$categoryDAO = $daoFactory->getCategoryDAO();
+$categories = $categoryDAO->findAll();
 
-//$categoryDAO = $daoFactory->getCategoryDAO();
-//$categories = $categoryDAO->findAll();
 
