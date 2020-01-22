5991ffb008953ebe24c5e54ce8c4a85f8af2e308
camol-huy <56692535+camol-huy@users.noreply.github.com>
2020-01-22 22:42:21 +0100

    new laoyut

--- scripts/homepage.php
+++ scripts/homepage.php
@@ -16,6 +16,15 @@ $movieDAO = $daoFactory->getMovieDAO();
 $lastMovies = $movieDAO->findLast($lastMoviesNumber);
 $popularMovies = $movieDAO->findBest($popularMoviesNumber);
 
+$allMovies = $movieDAO->findAll();
+$camol =  array_rand($allMovies,5);
+
+$category = $movieDAO->getCategory();
+$categoryName = $category->getName();
+
+
+
+
 
 //$tagDAO = $daoFactory->getTagDAO();
 //$tags = $tagDAO->findAll();
--- scripts/homepage.view.php
+++ scripts/homepage.view.php
@@ -3,11 +3,112 @@
 /**
  * script de vue (affichage du résultat) de la page d'accueil du site
  */
+
+
 ?>
 
-<h1>Welcome to w2w (index page).</h1>
+<?php if (isset($allMovies) && is_array($allMovies)) : ?>
+<!-- start of carousel -->
+<section id="carousel">
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+        <div class="carousel-inner ">
+            <div class="carousel-item active">
+                <img src="/assets/img/movies/carousel/<?php echo $allMovies[$camol[0]]->getId(); ?>.jpg" class="d-block w-100 movie_affiche" alt="...">
+                <div class="carousel-caption movie_title row">
+                    <?php echo $allMovies[$camol[0]]->getTitle();?>
+                </div>
 
+                <div class="carousel-caption movie_data">
+                    <a class="movie_data" href="#"><?php echo $allMovies[$camol[0]]->getYear();?></a> <!-- un lien vers tout les films sortis en 2018 -->
+                    <span class="movie_data">|</span>
+                    <a class=" movie_data" href="http://www.google.be"><?php echo $allMovies[$camol[0]]->getCategory() ;?></a> <!-- un lien vers tout les films sortis de la catégorie -->
+                    <span class="movie_data">|</span>
+                </div>
 
+                <div>
+                    <a class="carousel-caption movie_review" href="#">SEE OUR REVIEW</a> <!-- un lien vers la fiche du film -->
+                </div>
+            </div>
+
+            <div class="carousel-item">
+                <img src="/assets/img/movies/carousel/<?php echo $allMovies[$camol[1]]->getId(); ?>.jpg" class="d-block w-100 movie_affiche" alt="...">
+                <div class="carousel-caption movie_title row">
+                    <?php echo $allMovies[$camol[1]]->getTitle();?>
+                </div>
+
+                <div class="carousel-caption movie_data">
+                    <a class="movie_data" href="#"><?php echo $allMovies[$camol[1]]->getYear();?></a> <!-- un lien vers tout les films sortis en 2018 -->
+                    <span class="movie_data">|</span>
+                </div>
+
+                <div>
+                    <a class="carousel-caption movie_review" href="#">SEE OUR REVIEW</a> <!-- un lien vers la fiche du film -->
+                </div>
+            </div>
+
+            <div class="carousel-item">
+                <img src="/assets/img/movies/carousel/<?php echo $allMovies[$camol[2]]->getId(); ?>.jpg" class="d-block w-100 movie_affiche" alt="...">
+                <div class="carousel-caption movie_title row">
+                    <?php echo $allMovies[$camol[2]]->getTitle();?>
+                </div>
+
+                <div class="carousel-caption movie_data">
+                    <a class="movie_data" href="#"><?php echo $allMovies[$camol[2]]->getYear();?></a> <!-- un lien vers tout les films sortis en 2018 -->
+                    <span class="movie_data">|</span>
+                </div>
+
+                <div>
+                    <a class="carousel-caption movie_review" href="#">SEE OUR REVIEW</a> <!-- un lien vers la fiche du film -->
+                </div>
+            </div>
+
+            <div class="carousel-item">
+                <img src="/assets/img/movies/carousel/<?php echo $allMovies[$camol[3]]->getId(); ?>.jpg" class="d-block w-100 movie_affiche" alt="...">
+                <div class="carousel-caption movie_title row">
+                    <?php echo $allMovies[$camol[3]]->getTitle();?>
+                </div>
+
+                <div class="carousel-caption movie_data">
+                    <a class="movie_data" href="#"><?php echo $allMovies[$camol[3]]->getYear();?></a> <!-- un lien vers tout les films sortis en 2018 -->
+                    <span class="movie_data">|</span>
+                </div>
+
+                <div>
+                    <a class="carousel-caption movie_review" href="#">SEE OUR REVIEW</a> <!-- un lien vers la fiche du film -->
+                </div>
+            </div>
+
+            <div class="carousel-item">
+                <img src="/assets/img/movies/carousel/<?php echo $allMovies[$camol[4]]->getId(); ?>.jpg" class="d-block w-100 movie_affiche" alt="...">
+                <div class="carousel-caption movie_title row">
+                    <?php echo $allMovies[$camol[4]]->getTitle();?>
+                </div>
+
+                <div class="carousel-caption movie_data">
+                    <a class="movie_data" href="#"><?php echo $allMovies[$camol[4]]->getYear();?></a> <!-- un lien vers tout les films sortis en 2018 -->
+                    <span class="movie_data">|</span>
+                    <a class=" movie_data" href="http://www.google.be"><?php echo $allMovies[$camol[4]]->getCategory() ;?></a> <!-- un lien vers tout les films sortis de la catégorie -->
+                    <span class="movie_data">|</span>
+                </div>
+
+                <div>
+                    <a class="carousel-caption movie_review" href="#">SEE OUR REVIEW</a> <!-- un lien vers la fiche du film -->
+                </div>
+            </div>
+
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</section>
+<!-- end CAROUSEL -->
+<?php endif; ?>
 
 <h2>Last movies :</h2>
 <?php if (isset($lastMovies) && is_array($lastMovies)) : ?>
