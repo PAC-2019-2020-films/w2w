f8f80c1c8c92085233e364e59cfa419d730ffabb
mikaellr <mikaellr@laposte.net>
2020-02-06 16:45:19 +0100

    Début gestion films par admins (bis)

--- scripts/admin/index.php
+++ scripts/admin/index.php
@@ -2,3 +2,10 @@
 checkAdmin();
 ?>
 <h1>index partie admin</h1>
+
+
+<ul>
+    <li><a href="/admin/movie-list.php">Liste des films</a></li>
+    <li><a href="/admin/movie-add.php">Ajouter un film</a></li>
+    <li><a href="/admin/user-list.php">Liste des utilisateurs</a></li>
+</ul>
