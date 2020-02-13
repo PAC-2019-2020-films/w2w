489d0a014dbbe2fcd83c9f0eca3d435e8b5795e5
Olivier CAMUS <olivier_camus@hotmail.com>
2020-02-13 22:33:36 +0100

    all_movies has added

--- scripts/all_movies.view.php
+++ scripts/all_movies.view.php
@@ -1,15 +1,3 @@
-<?php
-
-use \w2w\DAO\DAOFactory;
-
-checkAdmin();
-
-$daoFactory = DAOFactory::getDAOFactory();
-$movieDAO = $daoFactory->getMovieDAO();
-$movies = $movieDAO->findAll();
-
-
-?>
 <!-- start of image de siege de cinema -->
 <div class="page-title " style="background :url('/uploads/salle_cinema.jpg');">
 
@@ -21,26 +9,51 @@ $movies = $movieDAO->findAll();
 
 </div>
 <!-- end of image de siege de cinema -->
-<div style="background-color: #f6f8fe">
-<div class="container movie_list">
-<div class="dominique">
-        <?php if (isset($movies) && is_array($movies) && count($movies) > 0) : ?>
-            <?php foreach ($movies as $movie) : ?>
-    <div class="row p-3">
-        <!-- TODO : aligner en responsive -->
-                <div class="col-md-2 text-center">
-                    <img class="img-responsive" src="/uploads/<?php echo escape($movie->getPoster()); ?>-medium.jpg" alt=""
-                         style="max-width: 100px">
-                </div>
-                <div class="col-md-7 laura">
-                    <h4><a href="/admin/movie-edit.php?id=<?php echo escape($movie->getId()); ?>"><?php echo escape($movie->getTitle()); ?></a></h4>
-                </div>
-                <div class="col-md-3 laura" >
-                    <h5><a href="/admin/movie-edit.php?id=<?php echo escape($movie->getId()); ?>"><?php echo escape($movie->getRating()->getName()); ?></a></h5>
-                </div>
+    <div class="pt-4" style="background-color: #f6f8fe">
+        <div class="container movie_list">
+            <div class="dominique">
+
+                    <?php foreach ($movies as $movie) : ?>
+                        <div class="row p-4">
+                            <!-- TODO : aligner en responsive -->
+                            <div class="col-md-2 text-center">
+                                <img class="img-responsive" src="/uploads/<?php echo escape($movie->getPoster()); ?>-medium.jpg" alt=""
+                                     style="max-width: 100px">
+                            </div>
+                            <div class="col-md-7 laura">
+                                <h4><a href="/admin/movie-edit.php?id=<?php echo escape($movie->getId()); ?>"><?php echo escape($movie->getTitle()); ?></a></h4>
+                            </div>
+                            <div class="col-md-3 laura" >
+                                <h5><a href="/admin/movie-edit.php?id=<?php echo escape($movie->getId()); ?>"><?php echo escape($movie->getRating()->getName()); ?></a></h5>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+            </div>
+        </div>
     </div>
-            <?php endforeach; ?>
-        <?php endif; ?>
-</div>
-</div>
-</div>
+
+
+
+    <?php
+
+if ($maxPages > 1) {
+    ?>
+    <ul class="pagination">
+        <li class="page-item<?php if ($page == 1) { ?>  disabled <?php } ?>">
+            <a href="all_movies.php?page=<?= $prevPage < 1 ? 1 : $prevPage ?>" class="page-link "> << </a></li>
+        <?php
+        for ($i = 1; $i <= $maxPages; $i++) {
+            ?>
+            <li><a href="all_movies.php?page=<?= $i ?>" class="page-link"><?= $i ?></a></li>
+            <?php
+        }
+        ?>
+        <li class="page-item<?php if ($page == $maxPages) { ?>  disabled <?php } ?>">
+            <a href="all_movies.php?page=<?= $nextPage > $maxPages ? $maxPages : $nextPage ?>" class="page-link">
+                >>
+            </a></li>
+    </ul>
+    <?php
+
+} ?>
+
