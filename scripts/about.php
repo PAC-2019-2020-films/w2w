07bde7094fab84d19aa1e0580c46337638d03590
LauraRst <roost.laura@gmail.com>
2020-04-06 20:49:59 +0200

    Design page About

--- scripts/about.php
+++ scripts/about.php
@@ -1,13 +1,30 @@
+<div class="header-page">
+    <div class="container">
+        <h2 class="header-page-tile text-center">A propos</h2>
+
+        <p class="small ">
+            <span class="">
+                <a href="/" class=" text-white">Accueil</a> &raquo;
+            </span>
+            <span class="">
+                <a href="/contact.php" class=" text-white">A propos</a>
+            </span>
+        </p>
+    </div>
+</div>
+<div class="container">
+    <div class="light-bg p-4">
 <h1>About W2W</h1>
 
 <div>
-    <p>What To Watch est un site de partage de critique de films. Il s'agit d'un projet de groupe en PHP dans le cadre d'une formation en informatique de gestion.</p>
+    <p>What To Watch est un site qui propose des critiques de films selon une échelle de cotation simple
+        à 4 degrés – navet, neutre, bon film et chef d’œuvre.</p>
+    <p>Il s'agit d'un projet de groupe en PHP dans le cadre d'une formation en informatique de gestion.</p>
+    <a href="" class="btn btn-primary">L'équipe</a>
+    <a href="" class="btn btn-secondary">Nous contacter</a>
+
 </div>
 
-<div>
-    <a href="/team.php">L'équipe.</a>
-</div>
 
-<div>
-    <a href="/contact.php">Nous contacter.</a>
+    </div>
 </div>
